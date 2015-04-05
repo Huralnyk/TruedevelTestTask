3dec95b28c15f0ee72426aba1fd4bf743db421af
Huralnyk <alexey.huralnyk@gmail.com>
2015-04-05 20:32:27 +0300

    Initial Commit

---
+++ TruedevelTestTask/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TruedevelTestTask
+//
+//  Created by Alexey Huralnyk on 4/5/15.
+//  Copyright (c) 2015 Alexey Huralnyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
